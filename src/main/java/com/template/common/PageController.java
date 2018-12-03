d3b06fd6798efc45f9e44b039ee61e40637d20ae
SouL's Macbook <soulate@gmail.com>
2018-12-04 01:58:34 +0900

    [MOD] Remove not use Class

--- src/main/java/com/template/common/PageController.java
+++ src/main/java/com/template/common/PageController.java
@@ -15,8 +15,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.util.WebUtils;
-
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Locale;
